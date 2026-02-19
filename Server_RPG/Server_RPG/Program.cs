c409d453ccabe76b6b1d1bd087a98f578746302f
Jin Wuk <jdk010705@gmail.com>
2026-02-19 21:50:26 +0900

    feat: DB 연결 설정 완료 및 appsettings 보안 처리

--- Server_RPG/Server_RPG/Program.cs
+++ Server_RPG/Server_RPG/Program.cs
@@ -1,25 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Server_RPG.Data; // AppDbContext가 있는 네임스페이스 확인 필요
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// 1. 서비스 등록
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// 2. DB 연결 문자열 가져오기 (User Secrets 또는 appsettings에서 조회)
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// 3. MySQL DbContext 등록
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
+// 4. HTTP 요청 파이프라인 설정
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
